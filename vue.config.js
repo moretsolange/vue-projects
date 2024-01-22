62fb118934dfb34624656757ed53ea13a2505a88
brittythed00m <moretimea@outlook.com>
2024-01-22 18:16:47 +0100

    name fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/vue-project/dist/' : '/',
+    process.env.NODE_ENV === 'production' ? '/vue-projects/dist/' : '/',
 };

058cd2c5fc94615c82874f17f3381063edea188f
brittythed00m <moretimea@outlook.com>
2024-01-22 17:44:09 +0100

    router fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
-
+import router from './router.js';
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
 
 const app = createApp(App);
 
+app.use(router);
 app.component('base-card', BaseCard);
 
 app.mount('#app');
